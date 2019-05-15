8bcf2f420ee0509300af801ad59763793afbdb46
chenkang <lv23ck@hotmail.com>
2019-05-15 11:13:45 +0800

    make blur_git to detect pair image of blur and unblur

--- final_lowlevel_git/blur/blur_git.py
+++ final_lowlevel_git/blur/blur_git.py
@@ -3,19 +3,34 @@ import cv2
 import numpy
 import BlurDetection
 import glob
+import BlurDetection.scripts as scripts
+import shutil
 
-img_path = raw_input("Please Enter Image Path: ")
+'''
+img_path = input("Please Enter Image Path: ")
 assert os.path.exists(img_path), "img_path does not exists"
+'''
+args = scripts.get_args()
+img_path = args.image
 
 image_data1 = glob.glob(img_path + '/*.jpg')
-a = []
+unblur, blur = [], []
 for i in image_data1:
 
-    img = cv2.imread(i)
-    img_fft, val, blurry = BlurDetection.blur_detector(img)
-    a.append(val)
-    print(a)
-    print blurry
+    img1 = cv2.imread(i)
+    img_fft1, val1, blurry1 = BlurDetection.blur_detector(img1)
+    unblur.append(val1)
 
-    print "this image {0} blurry".format(["isn't", "is"][blurry])
+    img2 = cv2.imread(i.replace('blur0', 'blur1'))
+    img_fft2, val2, blurry2 = BlurDetection.blur_detector(img2)
+    blur.append(val2)
+    #print(a)
+    #print(blurry)
+    '''
+    if blurry:
+        shutil.move(i, '/home/chk/Documents/data/train_code/URL_image/blur_test/blur')
+    else:
+        shutil.move(i, '/home/chk/Documents/data/train_code/URL_image/blur_test/not_blur')
+    '''
+    print("{0} {1} blurry".format(i, ["isn't", "is"][blurry]))
 
